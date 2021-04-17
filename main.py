49030d3a69095a3fbd68694b000297f6a8634b66
john_doe <2429822053@qq.com>
2021-04-17 17:51:48 +0800

    init

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ from window import Ui_Form
 from syn_flood import syn_flood
 
 
+def check_format(target_ip, target_port, thread_num):
+    ip_reg = r'^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$'
+    bytes_reg = r'^\d+$'
+    ip_flag = re.match(ip_reg, target_ip)
+    port_flag = re.match(bytes_reg, target_port)
+    thread_flag = re.match(bytes_reg, thread_num)
+    if ip_flag and port_flag and thread_flag and int(target_port) < 65536:
+        return True
+    else:
+        return False
+
+
 class MyWindow(QMainWindow, Ui_Form):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
@@ -16,23 +28,12 @@ class MyWindow(QMainWindow, Ui_Form):
         self.stop_button.clicked.connect(self.stop)
         self.stop_threads = False
 
-    def check_format(self, target_ip, target_port, thread_num):
-        ip_reg = r'^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$'
-        bytes_reg = r'^\d+$'
-        ip_flag = re.match(ip_reg, target_ip)
-        port_flag = re.match(bytes_reg, target_port)
-        thread_flag = re.match(bytes_reg, thread_num)
-        if ip_flag and port_flag and thread_flag and int(target_port) < 65536:
-            return True
-        else:
-            return False
-
     def start(self):
         target_ip = self.target_ip.text()
         target_port = self.target_port.text()
         thread_num = self.thread_num.text()
         if target_ip and target_port and thread_num:
-            if self.check_format(target_ip, target_port, thread_num):
+            if check_format(target_ip, target_port, thread_num):
                 target_port = int(target_port)
                 thread_num = int(thread_num)
                 self.info.setText('ATTACKING...')
